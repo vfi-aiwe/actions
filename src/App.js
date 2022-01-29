87f9730c111319316d5554610f5325f8081bc7a1
vfi-aiwe <vfi@aiweapps.com>
2022-01-29 13:45:13 +0300

    f

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.Try to deploy with actions
+          Edit <code>src/App.js</code> and save to reload.Try to deploy with actions NEw deploy
         </p>
         <a
           className="App-link"
